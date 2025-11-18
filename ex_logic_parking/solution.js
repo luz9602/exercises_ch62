203cc60efe73d67010fd9bacba57c4a229f5e576
Luz Mora <luz9602@gmail.com>
2025-11-17 18:05:46 -0600

    chore: translating code

--- ex_logic_parking/solution.js
+++ ex_logic_parking/solution.js
@@ -1,12 +1,12 @@
 //definir variables
-let horas=0
-const precio_horas=15
-const descuento=20
+let hours=0
+const price_hours=15
+const discount=20
 let total=0
 
-horas=7 //suponiendo que estas fueran las horas que nos proporcione el usuario
-total=horas*precio_horas
-if(horas>=3){
-    total=total-descuento
+hours=7 // simulated user input
+total=hours*price_hours
+if(hours>=3){
+    total=total-discount
 }
 console.log(total)
